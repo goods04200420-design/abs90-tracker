db43505c63488439f0b7ae19207680b2d27da50b
goods0420 <goods04200420@gmail.com>
2026-07-18 08:55:27 +0900

    첫 체성분 측정을 체중 추세 기준으로 적용

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='abs90-v9';
+const CACHE='abs90-v10';
 const ASSETS=['./index.html','./login.html','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
